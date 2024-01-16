f0191e6c5c266223ffede7b4ee89a8ac6d087003
sanguine6660 <sanguine6660@gmail.com>
2024-01-17 00:48:47 +0100

    Update landing page

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -17,14 +17,56 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+    const [emojiPaths, setEmojiPaths] = useState<string[]>([]);
+
+    useEffect(() => {
+        const importAll = async () => {
+            const files = import.meta.glob('./emojis/*.png');
+
+            const paths = await Promise.all(
+                Object.keys(files).map(async (path) => {
+                    const imported = await files[path]();
+                    if (imported && typeof imported === 'object' && 'default' in imported) {
+                        return (imported as { default: string }).default;
+                    }
+                    return '';
+                })
+            );
+
+            setEmojiPaths(paths);
+        };
+
+        importAll();
+    }, []);
+
     return (
-        <header className="flex flex-col items-center justify-center">
-            <section className="flex w-screen flex-col items-center justify-center p-3 pt-8">
-                {/* TODO nice landing page */}
-                <Link to="/create">➕ Emoji » {'(TODO: add a nice landing page ;))'}</Link>
+        <header className="mt-10 flex h-full flex-col items-center justify-center">
+            <section className="flex h-full w-screen flex-col items-center justify-evenly p-3  pt-8 md:flex-row">
+                <div className="flex h-full flex-col items-center justify-evenly rounded-md border border-[var(--main-color)] bg-gray-400 bg-opacity-10 bg-clip-padding backdrop-blur-sm backdrop-filter">
+                    <h1 className="m-4 text-2xl font-bold text-[var(--main-color)]">Showcase</h1>
+                    <div className="grid grid-cols-6">
+                        {emojiPaths.map((path, index) => (
+                            <img key={index} src={path} alt={`emoji-${index}`} className="m-2 h-[50px] w-[50px]" />
+                        ))}
+                    </div>
+                </div>
+                <div className="flex h-full flex-col items-center justify-evenly">
+                    <h1 className="m-4 text-2xl font-bold text-[var(--main-color)]">Voxify Emoji Maker</h1>
+                    <p className="text-md m-4 font-semibold text-[var(--description-color)]">Create Emojis for you Discord server with ease</p>
+                    <hr className="my-2 h-1 w-full border-0 bg-gray-400" />
+                    <Link
+                        className="group relative mb-2 me-2 mt-4 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-cyan-500 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-cyan-200 group-hover:from-cyan-500 group-hover:to-blue-500 dark:text-white dark:focus:ring-cyan-800"
+                        to="/create"
+                    >
+                        <span className="relative rounded-md bg-gray-900 px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0">
+                            ➕ Start Creating New Discord Emojis Now!
+                        </span>
+                    </Link>
+                </div>
             </section>
         </header>
     );
